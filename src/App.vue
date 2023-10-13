126b3da7a9530b2e8b3808bdc7db63a005e96291
berleon <github@leon-sixt.de>
2023-10-13 09:28:52 +0200

    feat: integrate UrlInput into App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import UrlInput from "./components/UrlInput.vue";
+
+import theSunBrexitStudentLoans from "./fixtures/the-sun-brexit-student-loans.json";
+
+interface ArticleContent {
+  title: string;
+  content: string;
+  url: string;
+}
+
+async function getPageContent(url: string): Promise<ArticleContent> {
+  // use fixture for now
+  // todo: fetch from url
+  url; // unused
+  return theSunBrexitStudentLoans;
+}
+
+async function onUrlEntered(url: string): Promise<void> {
+  const content = await getPageContent(url);
+  console.log(content);
+}
 </script>
 
 <template>
+  <UrlInput @url-entered="onUrlEntered" />
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
